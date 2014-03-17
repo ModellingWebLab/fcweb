eb1fae2a6baea5ec118fc51ee1fc0bdebfdc166f
Jonathan Cooper <jonathan.cooper@cs.ox.ac.uk>
2014-03-17 15:56:16 +0000

    Revert slanted column headings for matrix (see issue #40): the active link area no longer sits over the text, making selecting the protocol for viewing tricky!
    This reverts commit cad2c28a4c7ff79e8b25e4f99f00b7b1d0a76f2a.

--- src/main/webapp/res/js/db.js
+++ src/main/webapp/res/js/db.js
@@ -184,7 +184,7 @@ function drawMatrix (matrix)
 		table.appendChild (tr);
 		for (var col = -1; col < mat[0].length; col++)
 		{
-			var td = row == -1 ? document.createElement("th") : document.createElement("td");
+			var td = document.createElement("td");
 			tr.appendChild (td);
 			
 			//console.log ("row " + row + " col " + col);
