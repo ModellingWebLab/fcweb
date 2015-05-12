aa4c4a5cc2b0f128f71540cdbbc7549a9752cce3
Jonathan Cooper <jonathan.cooper@cs.ox.ac.uk>
2015-05-12 16:03:55 +0100

    Enable ACKS_LATE and tidy some comments on the Celery config (see issue #45).

--- resources/cgi-bin/fcws/celeryconfig.py
+++ resources/cgi-bin/fcws/celeryconfig.py
@@ -8,6 +8,7 @@ CELERY_ENABLE_UTC = True
 # We expect to have few tasks, but long running, so don't reserve more than you're working on
 # (this works well combined with the -Ofair option to the workers)
 CELERYD_PREFETCH_MULTIPLIER = 1
+CELERY_ACKS_LATE = True
 # Since tasks are long-running, we want to know if they are actually running
 CELERY_TRACK_STARTED = True
 
@@ -15,15 +16,13 @@ CELERY_TRACK_STARTED = True
 CELERYD_MAX_TASKS_PER_CHILD = 50
 
 CELERYD_TASK_SOFT_TIME_LIMIT = 60 * 60 * 15  # 15 hours
-
 # TODO: Check default time limits; should probably differ for admin & normal users
 # can set with decorator: @app.task(soft_time_limit=) or config CELERYD_TASK_SOFT_TIME_LIMIT
-# Use ignore_result? How about being able to kill tasks when expt deleted?  'revoked' http://docs.celeryproject.org/en/latest/userguide/workers.html#id8
-#   Do we need terminate? Is there any other way of stopping a running task?
-# We don't need to store results, since the task will callback to the front-end
-# But may need backend anyway for tracking state?
+# Also need to look into creating per-user queues dynamically - tricky bit is getting a worker to consume them!
 
+# We need a result backend to track task status.
+# However we don't need to store results, since the tasks will callback to the front-end.
 CELERY_RESULT_BACKEND = 'amqp'
-#CELERY_TASK_RESULT_EXPIRES = 18000  # 5 hours
 CELERY_IGNORE_RESULT = True
+
 CELERY_DISABLE_RATE_LIMITS = True
