57398b55a1c42aa120d6f8a11d8dc586580f3c8b
Jonathan Cooper <jonathan.cooper@cs.ox.ac.uk>
2015-07-27 11:48:04 +0100

    Tidy celery configuration, with some extra comments.

--- resources/cgi-bin/fcws/celeryconfig.py
+++ resources/cgi-bin/fcws/celeryconfig.py
@@ -1,9 +1,16 @@
-BROKER_URL = 'amqp://guest@localhost'
+# The settings in this file shouldn't need to be changed on different installations.
+# See /etc/default/celeryd for node-specific configuration.
 
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_ACCEPT_CONTENT=['json']
 CELERY_TIMEZONE = 'Europe/London'
 CELERY_ENABLE_UTC = True
+
+# Default broker to contact (if not overridden in CELERYD_OPTS)
+BROKER_URL = 'amqp://guest@localhost'
+
+# Default queue name to use
+CELERY_DEFAULT_QUEUE="default"
 
 # We expect to have few tasks, but long running, so don't reserve more than you're working on
 # (this works well combined with the -Ofair option to the workers)
@@ -17,7 +24,7 @@ CELERYD_MAX_TASKS_PER_CHILD = 50
 
 CELERYD_TASK_SOFT_TIME_LIMIT = 60 * 60 * 15  # 15 hours
 # TODO: Check default time limits; should probably differ for admin & normal users
-# can set with decorator: @app.task(soft_time_limit=) or config CELERYD_TASK_SOFT_TIME_LIMIT
+# can set with decorator: @app.task(soft_time_limit=) or config CELERYD_TASK_SOFT_TIME_LIMIT or as soft_time_limit option to apply_async.
 # Also need to look into creating per-user queues dynamically - tricky bit is getting a worker to consume them!
 
 # We need a result backend to track task status.
@@ -25,4 +32,5 @@ CELERYD_TASK_SOFT_TIME_LIMIT = 60 * 60 * 15  # 15 hours
 CELERY_RESULT_BACKEND = 'amqp'
 CELERY_IGNORE_RESULT = True
 
+# We don't make use of rate limiting, so turn it off for a performance boost
 CELERY_DISABLE_RATE_LIMITS = True
